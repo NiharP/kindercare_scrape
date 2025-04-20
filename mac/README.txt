ea75c50f299101acab189e3eacb4ac96ae4bac0c
Nihar Patil <nihar@NiharPatils-MacBook-Pro.local>
2025-04-20 16:35:36 -0700

    Added mac version of script

---
+++ mac/README.txt
@@ -0,0 +1,95 @@
+# KinderCare Media Downloader
+
+This script downloads all images and videos from the KinderCare parental portal for your child's account. It allows you to keep a personal archive of your child's memories and activities.
+
+## Prerequisites
+
+- Python 3.6 or higher
+- Git (for cloning the repository)
+- Chrome browser (for the cookie extension)
+
+## Setup Instructions
+
+1. **Clone or download this repository**
+
+   ```bash
+   git clone https://github.com/NiharP/kindercare_scrape.git
+   cd kindercare_scrape/
+
+   If you are using mac,
+   cd mac
+
+   If you are using linux,
+   cd linux
+   ```
+
+2. **Install Dependencies**
+
+   Using the included Makefile:
+   ```bash
+   make setup
+   ```
+
+2. **Install the "Get cookies.txt LOCALLY" Chrome Extension**
+
+   - Go to the Chrome Web Store and search for "Get cookies.txt LOCALLY" or use this link: [Get cookies.txt LOCALLY](https://chrome.google.com/webstore/detail/get-cookiestxt-locally/cclelndahbckbenkjhflpdbgdldlbecc)
+   - Click "Add to Chrome" to install the extension
+   - You should see a cookie icon in your browser extensions area
+
+3. **Export Cookies from KinderCare Portal**
+
+   - Log in to your KinderCare parental portal
+   - After successfully logging in, click on the "Get cookies.txt LOCALLY" extension icon
+   - In the popup window, click on "Export" to download the cookies.txt file
+   - Save the cookies.txt file in the same directory as the script
+
+## Usage
+
+1. **Place your cookies.txt file in the project directory**
+
+2. **Run the script using the Makefile:**
+
+   With default settings:
+   ```bash
+   make all
+   ```
+## Output
+
+The script will create a folder structure like:
+
+```
+kindercare_media/
+├── childid/
+│   ├── 20230115_01234.jpg
+└── child_name_2/
+    └── 20230115_01234.jpg
+```
+
+## Troubleshooting
+
+- **Authentication Issues**: Make sure you've exported cookies while logged in to the KinderCare portal
+- **Download Errors**: Check your internet connection and try again
+- **Permission Errors**: Ensure the script has write permissions to the output directory
+- **Dependency Issues**: Run `make setup` to ensure all dependencies are installed correctly
+- **Makefile Errors**: On Windows, you may need to install Make separately or use the Python commands directly
+
+## Project Structure
+
+```
+kindercare-downloader/
+├── kindercare_downloader.sh  # Main script
+├── Makefile                  # Automation for common tasks
+├── README.md                 # This file
+└── cookies.txt               # Your exported cookies (you need to create this)
+```
+
+## Notes
+
+- This script respects KinderCare's servers by using reasonable delays between requests
+- Only use this to download your own child's media for personal use
+- Your login session may expire after some time, requiring you to generate a new cookies.txt file
+- For help with available commands, run `make help`
+
+## Disclaimer
+
+This tool is for personal use only to download your own child's media content. Please respect KinderCare's terms of service and only use this script for personal archiving purposes.
---
+++ mac/kindercare_downloader.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# child id see Manage Children page: https://classroom.kindercare.com/accounts
+# ID is in the url : https://classroom.kindercare.com/accounts/xxxxxx 
+
+child_id="589168"
+no_insert_db=0
+caption=0
+ 
+usage() {
+        echo "Usage: $0 [ -cik xxxxxx ] " 1>&2
+}
+
+exit_abnormal() {
+        usage
+        exit 1
+}
+
+while getopts "cik:" o; do
+	case "${o}" in
+		c)
+			caption=1
+			;;
+		i)
+			no_insert_db=1
+			;;
+		k)
+			child_id=${OPTARG}
+			;;
+		:)
+			echo "Error: -${OPTARG} requires an argument."
+			exit_abnormal
+			;;
+		*)
+			exit_abnormal
+			;;
+	esac
+done
+
+if [ ! -s "$(dirname "${0}")/${child_id}/id.db" ]; then
+  mkdir -p "$(dirname "${0}")/${child_id}"
+  mkdir -p "/tmp/_${child_id}"
+  touch "$(dirname "${0}")/${child_id}/id.db"
+else
+  mkdir -p "/tmp/_${child_id}"
+fi
+
+done=0
+k=1
+
+until [ $done == "1" ]; do
+	wget -q --load-cookies $(dirname ${0})/cookies.txt "https://classroom.kindercare.com/accounts/${child_id}/journal_api?page=${k}" -O /tmp/_${child_id}/_act
+
+	jq '.intervals[] | .[].activity' /tmp/_${child_id}/_act > /tmp/_${child_id}/_jsontemp 
+
+	if [ -s /tmp/_${child_id}/_jsontemp ]; then
+		jq '.activity_file_id' /tmp/_${child_id}/_jsontemp  > /tmp/_${child_id}/_id 
+		jq '.title' /tmp/_${child_id}/_jsontemp > /tmp/_${child_id}/_title 
+		jq '.description' /tmp/_${child_id}/_jsontemp > /tmp/_${child_id}/_desc
+		jq '.created_at' /tmp/_${child_id}/_jsontemp > /tmp/_${child_id}/_date
+		jq '.image.big.url,.video.url' /tmp/_${child_id}/_jsontemp | grep -v null  > /tmp/_${child_id}/_img
+
+		sort /tmp/_${child_id}/_id > /tmp/_${child_id}/_id_new
+		
+		comm -13 "$(dirname "${0}")/${child_id}/id.db" "/tmp/_${child_id}/_id_new" > "/tmp/_${child_id}/_delta"
+
+		n=`cat /tmp/_${child_id}/_id | wc -l`
+
+		new=`cat /tmp/_${child_id}/_delta | wc -l`
+
+		if [ $new -gt 0 ]; then
+			i=1
+			until [ $i -gt $n ]; do
+				id=`head -$i /tmp/_${child_id}/_id | tail -1`  
+			 	test=`grep $id /tmp/_${child_id}/_delta`
+				
+				if [ ! -z "$test" ]; then
+					date=`head -$i /tmp/_${child_id}/_date | tail -1 | tr -d '\"'` 
+					date=$(echo "$date" | sed 's/\.[0-9]*//' | sed 's/:\([0-9][0-9]\)$/\1/')
+date=$(date -j -f "%Y-%m-%dT%H:%M:%S%z" "$date" "+%Y%m%d_%H%M%S")
+					desc=`head -$i /tmp/_${child_id}/_desc | tail -1 | tr -d '\"' | tr -dc '[:alnum:][ ][.!?]\n\r'`  
+					desc=${desc/"Look what I'm doing today!"/}
+					title=`head -$i /tmp/_${child_id}/_title | tail -1 | tr -d '\"' | tr -dc "[:alnum:][ ][.!?']\n\r"`  
+					title=${title/"Look what I'm doing today!"/}
+					img=`head -$i /tmp/_${child_id}/_img | tail -1 | tr -d '\"'`  
+					
+					vidtest=`echo $img | grep "activity_file/video"`
+					if [ -z "$vidtest" ]; then
+						wget -q $img -O ${child_id}/${date}.jpg
+						exiftool -XPcomment="$desc" -title="$title $desc" ${child_id}/${date}.jpg
+						if [ "$caption" == "1" ]; then
+							size=`exiftool -ImageSize ${child_id}/${date}.jpg | cut -d: -f2 | cut -dx -f1 | cut -d\  -f2`
+							convert ${child_id}/${date}.jpg -background lightgray -size ${size}x -pointsize 20 caption:"$title $desc" -gravity center -append result.jpg
+							mv -f result.jpg ${child_id}/${date}.jpg
+						fi
+					else
+						wget -q $img -O ${child_id}/${date}.mov
+						echo "converting video"
+						ffmpeg -loglevel panic -i ${child_id}/${date}.mov -metadata title="$title $desc" ${child_id}/${date}.mp4
+						rm ${child_id}/${date}.mov
+					fi
+				else
+					done=1
+				fi
+				((i++))
+			done
+		else
+			done=1
+		fi
+		
+		((k++))
+
+		if [ "$no_insert_db" == "0" ]; then
+			cat /tmp/_${child_id}/_delta >> $(dirname ${0})/${child_id}/id.db
+			sort -u $(dirname ${0})/${child_id}/id.db > /tmp/_${child_id}/id.db_sorted
+			mv /tmp/_${child_id}/id.db_sorted $(dirname ${0})/${child_id}/id.db
+		fi
+		rm -rf /tmp/_${child_id}/_act /tmp/_${child_id}/_id /tmp/_${child_id}/_title /tmp/_${child_id}/_desc /tmp/_${child_id}/_date /tmp/_${child_id}/_img /tmp/_${child_id}/_delta /tmp/_${child_id}/_jsontemp $(dirname ${0})/${child_id}/*.jpg_original
+	else
+		done=1
+	fi
+done
